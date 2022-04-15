bc79083629016d5af590ae87cbe0f5dbfe7dc6cc
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2022-04-15 19:35:58 +0300

    Added user & auth APIs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // app.enableCors();
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
+
+  const config = new DocumentBuilder()
+    .setTitle('Users')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addBearerAuth({ in: 'header', type: 'http', scheme: 'bearer', bearerFormat: 'Bearer' }, 'BearerToken')
+    .addTag('users')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
+
 bootstrap();
